e244e675ea75f2e7953ec4518dd98e2096bf321c
Ricardo Barbosa <ric.brasiliano@outlook.com>
2024-04-29 15:21:28 -0300

    create axios interceptor

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,10 +1,24 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
+import router from "../router/index";
 
 const api = axios.create({
   baseURL: "http://localhost:8080/api/v1/",
   withCredentials: true,
 });
+
+api.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      console.log(error);
+      router.push({ path: "/login" });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios;
